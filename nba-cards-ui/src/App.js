c9443b2acaf57a20ed089725948d45d48aebfede
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2022-05-18 16:27:21 -0400

    nbacard ui started

---
+++ nba-cards-ui/src/App.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from "react";
+import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
+
+function App() {
+  return
+
+  <>
+
+
+  </>
+}
+
+export default App;
---
+++ nba-cards-ui/src/NbaCard.js
@@ -0,0 +1,15 @@
+function NbaCard({nbaCard}) {
+    return <div className="card">
+        <img className="card-image"/>
+        <div className="card-body">
+            <h5 className="card-title">Player Name</h5>
+            <p>Team</p>
+            <p>Career ppg</p>
+            <p>Career apg</p>
+            <p>Career rpg</p>
+        </div>
+
+    
+    </div>
+}
+export default NbaCard;
