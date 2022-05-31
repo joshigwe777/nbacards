b79757f4c2916e43aac0998adb242fe912778178
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2022-05-31 14:11:58 -0400

    ui code finished

---
+++ nba-cards-ui/src/Heading.js
@@ -0,0 +1,13 @@
+import NavBar from "./NavBar";
+
+function Heading() {
+    return (
+        <>
+        <h1>Nba Player Cards</h1>
+        <NavBar/>
+        </>
+    )
+
+
+}
+export default Heading;
