50ee1a565f8fc114bbdb95cffab82c316c15ef6b
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2022-06-02 15:47:43 -0400

    validations completed

--- nba-cards-api/src/main/java/nbacards/Validations/StatsCannotBeLessThanZero.java
+++ nba-cards-api/src/main/java/nbacards/Validations/StatsCannotBeLessThanZero.java
@@ -1,4 +1,24 @@
 package nbacards.Validations;
 
-public class StatsCannotBeLessThanZero {
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target({TYPE,ANNOTATION_TYPE})
+@Retention(RUNTIME)
+@Constraint(validatedBy = {StatsCannotBeLessThanZeroValidator.class})
+@Documented
+public @interface StatsCannotBeLessThanZero {
+
+    String message() default "In order for statistics to be valid, they cannot be less than zero";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default{};
 }
--- nba-cards-api/src/main/java/nbacards/Validations/StatsCannotBeLessThanZeroValidator.java
+++ nba-cards-api/src/main/java/nbacards/Validations/StatsCannotBeLessThanZeroValidator.java
@@ -1,4 +1,42 @@
 package nbacards.Validations;
 
-public class StatsCannotBeLessThanZeroValidator {
+import nbacards.data.NbaCardRepository;
+import nbacards.models.NbaCard;
+
+import javax.validation.Constraint;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+public class StatsCannotBeLessThanZeroValidator implements ConstraintValidator<StatsCannotBeLessThanZero, NbaCard> {
+
+    private final NbaCardRepository repository;
+
+
+    public StatsCannotBeLessThanZeroValidator(NbaCardRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public boolean isValid(NbaCard card, ConstraintValidatorContext context) {
+        return statsAreValid(card);
+    }
+
+    private boolean statsAreValid(NbaCard card) {
+        if(card.getPpg()>=0 &&
+                card.getApg()>=0 &&
+                card.getRpg()>=0) {
+            return true;
+
+        }
+        return false;
+    }
 }
