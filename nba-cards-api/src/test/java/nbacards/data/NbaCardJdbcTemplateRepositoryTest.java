11b45ea23d9ac9fff853a598bbc15809ceea6abc
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2022-06-07 11:51:37 -0400

    repository test

--- nba-cards-api/src/test/java/nbacards/data/NbaCardJdbcTemplateRepositoryTest.java
+++ nba-cards-api/src/test/java/nbacards/data/NbaCardJdbcTemplateRepositoryTest.java
@@ -1,36 +1,125 @@
 package nbacards.data;
 
+import nbacards.models.NbaCard;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class NbaCardJdbcTemplateRepositoryTest {
 
-    @Test
-    void findAll() {
+    @Autowired
+    NbaCardRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
     }
 
     @Test
-    void findById() {
+    void shouldFindAllCards() {
+        List<NbaCard> cards = repository.findAll();
+        assertTrue(cards.size()>=3);
     }
 
     @Test
-    void findByPosition() {
+    void shouldFindLebronById() {
+        NbaCard lebron = repository.findById(1);
+        assertEquals("Lebron James", lebron.getName());
     }
 
     @Test
-    void findByTeam() {
+    void shouldNotFindKobeWhoIsAnInvalidPlayer() {
+
+        NbaCard kobe = repository.findById(5);
+        assertEquals(null, kobe);
+
     }
 
     @Test
-    void add() {
+    void shouldFindLukaDoncicByPosition() {
+        List<NbaCard> luka = repository.findByPosition("pg");
+        assertTrue(luka.stream().anyMatch(c->c.getName().equalsIgnoreCase("Luka Doncic")));
     }
 
     @Test
-    void update() {
+    void shouldNotFindChrisPaulByPosition() {
+        List<NbaCard> chris = repository.findByPosition("pg");
+        assertFalse(chris.stream().anyMatch(c->c.getName().equalsIgnoreCase("Chris Paul")));
     }
 
     @Test
-    void deleteById() {
+    void shouldFindLebronByTeam() {
+        List<NbaCard> lebron = repository.findByTeam(1);
+        assertTrue(lebron.stream().anyMatch(c->c.getName().equalsIgnoreCase("Lebron James")));
+    }
+
+    @Test
+    void shouldNotFindKobeByTeam() {
+        List<NbaCard> kobe = repository.findByTeam(1);
+        assertFalse(kobe.stream().anyMatch(c->c.getName().equalsIgnoreCase("Kobe Bryant")));
+
+    }
+
+    @Test
+    void shouldAddSteph() {
+        NbaCard stephCurry = new NbaCard();
+        stephCurry.setPpg(25.0);
+        stephCurry.setPosition("pg");
+        stephCurry.setName("Stephen Curry");
+        stephCurry.setTeamId(2);
+        stephCurry.setApg(6.0);
+        stephCurry.setRpg(5.5);
+
+        NbaCard added = repository.add(stephCurry);
+        assertEquals(added,stephCurry);
+
+    }
+
+    @Test
+    void shouldUpdateLebron() {
+        NbaCard lebronJames = new NbaCard();
+        lebronJames.setPpg(27.0);
+        lebronJames.setCardId(1);
+        lebronJames.setPosition("sf");
+        lebronJames.setName("Lebron James");
+        lebronJames.setTeamId(1);
+        lebronJames.setApg(6.0);
+        lebronJames.setRpg(5.5);
+
+
+        assertTrue(repository.update(lebronJames));
+
+    }
+    @Test
+    void shouldNotUpdateWithInvalidCardId() {
+        NbaCard lebronJames = new NbaCard();
+        lebronJames.setPpg(27.0);
+        lebronJames.setCardId(1);
+        lebronJames.setPosition("sf");
+        lebronJames.setName("Lebron James");
+        lebronJames.setTeamId(4);
+        lebronJames.setApg(6.0);
+        lebronJames.setRpg(5.5);
+
+
+        assertFalse(repository.update(lebronJames));
+
+    }
+
+    @Test
+    void shouldDeleteLukaById() {
+
+        assertTrue(repository.deleteById(2));
+        assertFalse(repository.deleteById(2));
+
     }
 }
--- nba-cards-api/src/test/java/nbacards/data/TeamJdbcTemplateRepositoryTest.java
+++ nba-cards-api/src/test/java/nbacards/data/TeamJdbcTemplateRepositoryTest.java
@@ -1,12 +1,32 @@
 package nbacards.data;
 
+import nbacards.models.Team;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class TeamJdbcTemplateRepositoryTest {
 
+    @Autowired
+    TeamRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+
     @Test
-    void findAll() {
+    void shouldFindAll() {
+        List<Team> teamList = repository.findAll();
+        assertTrue(teamList.size()>=3);
     }
 }
