3b536c67984a5230e88ff4ea0b0ef69f059ea010
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2022-06-06 21:51:01 -0400

    repository tests

--- nba-cards-api/src/test/java/nbacards/data/AppUserJdbcTemplateRepositoryTest.java
+++ nba-cards-api/src/test/java/nbacards/data/AppUserJdbcTemplateRepositoryTest.java
@@ -1,20 +1,36 @@
 package nbacards.data;
 
+import nbacards.models.AppUser;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class AppUserJdbcTemplateRepositoryTest {
+
+    @Autowired
+    AppUserRepository repository;
+
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
 
     @Test
     void shouldFindByUsername() {
+        AppUser user = repository.findByUsername("john@smith.com");
+
+        assertEquals("john@smith.com", user.getUsername());
     }
 
-    @Test
-    void shouldCreateValidCard() {
-    }
-
-    @Test
-    void shouldUpdate() {
-    }
+    
 }
